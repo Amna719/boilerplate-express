07d7f4cd6595bb3bc8868b09ec33e4b80043e369
Amna Amen <amnaamen13@gmail.com>
2021-11-10 19:15:56 -0500

    Middleware

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 var express = require('express');
 var app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use((req,res,next) => {
+    let output= `${req.method} ${req.path} - ${req.ip}`;
+    console.log(output);
+    next();
+});
 
  module.exports = app;
