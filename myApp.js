959ae2e546644a6f9c4f0426cfc42e1a73387fae
Amna Amen <amnaamen13@gmail.com>
2021-11-10 19:01:17 -0500

    Chain Middleware

--- myApp.js
+++ myApp.js
@@ -1,39 +1,14 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
+var bodyParser = require('body-parser');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/now', (req,res,next)=>{
+    req.time = new Date().toString();
+    next();
+  }, (req,res)=> {
+    res.send({time: req.time})
+    }
+  );
+  
  module.exports = app;
