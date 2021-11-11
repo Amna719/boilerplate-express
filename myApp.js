35769ba3ef6b59e2f30b2efd55b74b8e93b3b1b2
Amna Amen <amnaamen13@gmail.com>
2021-11-10 19:24:58 -0500

    body parser for POST request

--- myApp.js
+++ myApp.js
@@ -1,39 +1,16 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.post('/name', (req,res)=>{
+   var userName = req.body.first + " " + req.body.last;
+  res.json({ name: userName });
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
