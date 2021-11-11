96a87a5942d66d1a5ae3e21b6b399c84ce77c946
Amna Amen <amnaamen13@gmail.com>
2021-11-10 19:22:39 -0500

    Query Param from client

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 var express = require('express');
 var app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/name', (req,res)=>{
+    let firstName = req.query.first;
+    let lastName = req.query.last;
+    res.json({name: `${firstName} ${lastName}`})
+});
 
  module.exports = app;
